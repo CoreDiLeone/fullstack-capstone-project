98e28bfe9c9fadd34b4c91ef51f077f7408c3142
CoreDiLeone <fireejuegos@gmail.com>
2024-11-18 19:03:40 -0500

    Create the MainPage and Navbar

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -16,23 +16,11 @@ async function connectToDatabase() {
     const client = new MongoClient(url);      
 
     // Task 1: Connect to MongoDB
-   try{
 
-    await client.connect()
-     // Task 2: Connect to database giftDB and store in variable dbInstance
-    dbInstance = client.db(dbName);
-    console.log("La conexión con mongodb ha sido exitosa");
-     // Task 3: Return database instance
-    return dbInstance;
-   }catch(err){
-    console.error("No se ha podido establecer la conexión con la base de datos"+ err);
-   }
-
-     /*
     await client.connect();
     dbInstance = client.db(dbName);
     return dbInstance;
-     */
+     
 }
 
 module.exports = connectToDatabase;
