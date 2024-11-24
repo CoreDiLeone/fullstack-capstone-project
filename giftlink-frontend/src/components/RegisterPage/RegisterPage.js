e94dc06b4d0f5f5c736f858d14e7f273f6c15029
CoreDiLeone <fireejuegos@gmail.com>
2024-11-23 23:59:26 -0500

    Implement Back-end in Register Component

--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -1,87 +1,128 @@
-import React, { useState } from 'react';
+    import React, { useState } from 'react';
+    import {urlConfig} from '../../config';
+    import { useAppContext } from '../../context/AuthContext';
+    import { useNavigate } from 'react-router-dom';
+    import './RegisterPage.css';
 
-import './RegisterPage.css';
+    function RegisterPage() {
+        const [firstName, setFirstName] = useState('');
+        const [lastName, setLastName] = useState('');
+        const [email, setEmail] = useState('');
+        const [password, setPassword] = useState('');
 
-function RegisterPage() {
+       
+         const [showerr, setShowerr] = useState('');
 
-    //insert code here to create useState hook variables for firstName, lastName, email, password
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    // insert code here to create handleRegister function and include console.log
-    const handlerRegister = async () => {
-        console.log("Btn handlerRegistred cliked :)")
-    }
-    return (
-        <div className="container mt-5">
-            <div className="row justify-content-center">
-                <div className="col-md-6 col-lg-4">
-                    <div className="register-card p-4 border rounded">
-                        <h2 className="text-center mb-4 font-weight-bold">Register</h2>
+       
+        const navigate = useNavigate();
+        const { setIsLoggedIn } = useAppContext();
 
-                        {/* insert code here to create input elements for all the variables - firstName, lastName, email, password */}
-                        <div className="mb-4">
+        const handleRegister = async () => {
+            const response = await fetch(`${urlConfig.backendUrl}/api/auth/register`, {
+              
+                method: 'POST',
+               
+                headers: {
+                    'content-type': 'application/json',
+                },
+               
+                body: JSON.stringify({
+                    firstName: firstName,
+                    lastName: lastName,
+                    email: email,
+                    password: password
+                })
+            });
 
-                            <label htmlFor="firstName" className="form label"> FirstName</label><br />
-                            <input
-                                id="firstName"
-                                type="text"
-                                className="form-control"
-                                placeholder="Enter your firstName"
-                                value={firstName}
-                                onChange={(e) => setFirstName(e.target.value)}
-                            />
+           
+            const json = await response.json();
+            console.log('json data', json);
+            console.log('er', json.error);
+
+            
+            if (json.authtoken) {
+                sessionStorage.setItem('auth-token', json.authtoken);
+                sessionStorage.setItem('name', firstName);
+                sessionStorage.setItem('email', json.email);
+            
+                setIsLoggedIn(true);
+           
+                navigate('/app');
+            }
+            if (json.error) {
+           
+                setShowerr(json.error);
+            }
+        }
+
+        return (
+            <div className="container mt-5">
+                <div className="row justify-content-center">
+                    <div className="col-md-6 col-lg-4">
+                        <div className="register-card p-4 border rounded">
+                            <h2 className="text-center mb-4 font-weight-bold">Register</h2>
+                            <div className="mb-3">
+                                <label htmlFor="firstName" className="form-label">FirstName</label>
+                                <input
+                                    id="firstName"
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Enter your firstName"
+                                    value={firstName}
+                                    onChange={(e) => setFirstName(e.target.value)}
+                                />
+                            </div>
+
+                            {/* last name */}
+
+                            <div className="mb-3">
+                                <label htmlFor="lastName" className="form-label">LastName</label>
+                                <input
+                                    id="lastName"
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Enter your lastName"
+                                    value={lastName}
+                                    onChange={(e) => setLastName(e.target.value)}
+                                />
+                            </div>
+
+                            {/* email  */}
+                            <div className="mb-3">
+                                <label htmlFor="email" className="form-label">Email</label>
+                                <input
+                                    id="email"
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Enter your email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                            {/* Step 2 - Task 6*/}
+
+                                    <div className="text-danger">{showerr}</div>
+                            </div>
+
+                            <div className="mb-4">
+                                <label htmlFor="password" className="form-label">Password</label>
+                                <input
+                                    id="password"
+                                    type="password"
+                                    className="form-control"
+                                    placeholder="Enter your password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                            </div>
+                            <button className="btn btn-primary w-100 mb-3" onClick={handleRegister}>Register</button>
+                            <p className="mt-4 text-center">
+                                Already a member? <a href="/app/login" className="text-primary">Login</a>
+                            </p>
                         </div>
-                        <div className="mb-4">
-
-                            <label htmlFor="lastName" className="form label"> LastName</label><br />
-                            <input
-                                id="LastName"
-                                type="text"
-                                className="form-control"
-                                placeholder="Enter your LastName"
-                                value={lastName}
-                                onChange={(e) => setLastName(e.target.value)}
-                            />
-                        </div>
-
-                        <div className="mb-4">
-
-                            <label htmlFor="email" className="form label"> Email</label><br />
-                            <input
-                                id="email"
-                                type="text"
-                                className="form-control"
-                                placeholder="Enter your email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                        </div>
-                        <div className="mb-4">
-
-                            <label htmlFor="password" className="form label"> Password</label><br />
-                            <input
-                                id="password"
-                                type="text"
-                                className="form-control"
-                                placeholder="Enter your password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                        </div>
-                        {/* insert code here to create a button that performs the `handleRegister` function on click */}
-                        <button className="btn btn-primary w-100 mb-3" onClick={handlerRegister}>Register</button>
-                        <p className="mt-4 text-center">
-                            Already a member? <a href="/app/login" className="text-primary">Login</a>
-                        </p>
-
                     </div>
                 </div>
             </div>
-        </div>
+        );
+    }
 
-    )//end of return
-}
-
-export default RegisterPage;
+    export default RegisterPage;
