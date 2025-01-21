6cdd1e3dc45c1de9c50ba836775a9e8c2420ce0b
CoreDiLeone <fireejuegos@gmail.com>
2025-01-21 08:59:55 -0500

    Implement Back-end in Register Component

--- giftlink-frontend/src/components/RegisterPage/Register.js
+++ giftlink-frontend/src/components/RegisterPage/Register.js
@@ -1,6 +1,12 @@
 import { useState } from 'react';
 import './RegisterPage.css';
 import { useNavigate } from 'react-router-dom';
+//Task 1: Import urlConfig from `giftlink-frontend/src/config.js`
+import urlConfig from '../../config';
+//Task 2: Import useAppContext `giftlink-frontend/context/AuthContext.js`
+import { useAppContext } from '../../context/AuthContext';
+//Task 3: Import useNavigate from `react-router-dom` to handle navigation after successful registration.
+
 function RegisterPage() {
 
     const navigate = useNavigate();
@@ -10,12 +16,48 @@ function RegisterPage() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    // insert code here to create handleRegister function and include console.log
-    const handlerResgister = async (e) => {
-        e.preventDefault();
-        console.log("Register button clicked :)")
-        navigate("/app");
-    }
+    const [ errorMessage, setErrorMessage ] = useState("");
+
+    const { setIsLoggedIn } = useAppContext();
+
+    const handlerRegister = async () =>{
+       
+       try{ const response = await fetch(`${urlConfig.backendUrl}/api/auth/register`, 
+            { method: "POST",
+                headers: {
+                    "content-type" : "application/json"
+            },
+            body: JSON.stringify({
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                password: password
+            })
+
+        })
+        const jsonData = await response.json();
+        console.log("Response:", jsonData );
+        console.log("error:", jsonData.error);
+
+        if (jsonData.authtoken) {
+            sessionStorage.setItem('auth-token', jsonData.authtoken);
+            sessionStorage.setItem('name', jsonData.firstName);
+            sessionStorage.setItem('email', jsonData.email);
+        //Step 2 - Task 3
+            setIsLoggedIn(true);
+        //Step 2 - Task 4
+            navigate('/app');
+        }
+        if (jsonData.error) {
+        //Step 2 - Task 5
+            setErrorMessage(jsonData.error);
+        }
+         }catch(e){
+            console.log("There was an error in the request POST:", e.errorMessage)
+         }
+       
+     }
+   
 
     return (
         <div className="container mt-5">
@@ -70,7 +112,8 @@ function RegisterPage() {
                             />
                         </div>
                         {/* insert code here to create a button that performs the `handleRegister` function on click */}
-                        <button className="btn btn-primary w-100 mb-3" onClick={handlerResgister}> Register </button>
+                        <button className="btn btn-primary w-100 mb-3" onClick={handlerRegister}> Register </button>
+                        <div className="text-danger">{errorMessage}</div>
                         <p className="mt-4 text-center">
                             Already a member? <a href="/app/login" className="text-primary">Login</a>
                         </p>
